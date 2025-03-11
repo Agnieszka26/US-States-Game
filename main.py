aa2fcbed68b930ee35fe3868b58e99c901749efa
Agnieszka26 <medrek.agn@yahoo.com>
2025-03-11 09:03:46 +0100

    handle finish game, save states to learn

--- main.py
+++ main.py
@@ -23,18 +23,30 @@ turtle.shape(image)
 #4. Use a loop to allow the user to keep guessing
 #5. Record the correct guesses in a list
 #6. Keep track of the score
+#7. generate csv file with states names to learn
+
+def generate_csv_file_to_learn(correct_states, all_states):
+    states_to_learn_list = []
+    for s in all_states:
+        if s not in correct_states:
+            states_to_learn_list.append(s)
+    states_to_learn_table = pandas.DataFrame(states_to_learn_list)
+    states_to_learn_table.to_csv("states_to_learn.csv")
+
+
 def state_name_print(x,y):
     print_s=turtle.Turtle()
     print_s.hideturtle()
     print_s.penup()
-    print_s.goto((coordinate_x, coordinate_y))
+    print_s.goto((x,y))
     print_s.write(f"{answer_state}", align=ALIGNMENT, font=FONT)
+
+
 
 correct_user_guesses = []
 states_data = pandas.read_csv("50_states.csv")
 states_list = states_data.state.to_list()
 title="Guess the state"
-score = 0
 game_on = True
 
 while game_on:
@@ -42,6 +54,7 @@ while game_on:
 
     if answer_state == "Exit":
         print("Game Over!")
+        generate_csv_file_to_learn(correct_user_guesses,states_list)
         break
 
     for state in states_list:
@@ -50,7 +63,4 @@ while game_on:
             coordinate_y = states_data[states_data.state == answer_state].iloc[0]["y"]
             correct_user_guesses.append(answer_state)
             state_name_print(coordinate_x, coordinate_y)
-            score+=1
-            title=f"{score}/50 States correct!"
-
-turtle.mainloop()
+            title=f"{len(correct_user_guesses)}/50 States correct!"
